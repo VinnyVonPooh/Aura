e1cbf8496fd71ba2ee7c7972c61b08175a88e291
VinnyVonPooh <vinny.von.pooh@gmail.com>
2026-09-09 16:21:07 +0400

    13. Enemy Interface

--- Source/Aura/Characters/AuraEnemy.cpp
+++ Source/Aura/Characters/AuraEnemy.cpp
@@ -1,5 +1,13 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "AuraEnemy.h"
 
+void AAuraEnemy::HighlightActor()
+{
+	
+}
+
+void AAuraEnemy::UnHighlightActor()
+{
+	
+}
--- Source/Aura/Characters/AuraEnemy.h
+++ Source/Aura/Characters/AuraEnemy.h
@@ -4,12 +4,16 @@
 
 #include "CoreMinimal.h"
 #include "AuraCharacterBase.h"
+#include "Aura/Interaction/EnemyInterface.h"
 #include "AuraEnemy.generated.h"
 
 
 UCLASS()
-class AURA_API AAuraEnemy : public AAuraCharacterBase
+class AURA_API AAuraEnemy : public AAuraCharacterBase,public IEnemyInterface
 {
 	GENERATED_BODY()
 	
+public:
+	virtual void HighlightActor() override;
+	virtual void UnHighlightActor() override;
 };
