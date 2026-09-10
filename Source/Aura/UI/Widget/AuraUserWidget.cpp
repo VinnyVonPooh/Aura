8e3cdb56c08c45d8f84a8f4c2aacae081fcd60a4
VinnyVonPooh <vinny.von.pooh@gmail.com>
2026-09-10 17:01:19 +0400

    29. Aura User Widget and Widget Controller

---
+++ Source/Aura/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) MarmoDrake. All Rights Reserved.
+
+
+#include "AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
---
+++ Source/Aura/UI/WidgetController/AuraWidgetController.cpp
@@ -0,0 +1,5 @@
+// Copyright (c) MarmoDrake. All Rights Reserved.
+
+
+#include "AuraWidgetController.h"
+
