d5a29729ad29ae190f023b1b38d4c866093ef2b6
VinnyVonPooh <vinny.von.pooh@gmail.com>
2026-09-14 18:30:26 +0400

    39. Instant Gameplay Effects - добавление жизни/маны через сбор банок

--- Source/Aura/Actor/AuraEffectActor.cpp
+++ Source/Aura/Actor/AuraEffectActor.cpp
@@ -15,9 +15,9 @@ void AAuraEffectActor::BeginPlay()
 	Super::BeginPlay();
 }
 
-void AAuraEffectActor::ApplyEffectToTarget(AActor* Target, TSubclassOf<UGameplayEffect> GameplayEffectClass)
+void AAuraEffectActor::ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass)
 {
-	auto* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Target);
+	auto* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
 	if (!TargetASC) {
 		return;
 	}
--- Source/Aura/Actor/AuraEffectActor.h
+++ Source/Aura/Actor/AuraEffectActor.h
@@ -16,13 +16,12 @@ class AURA_API AAuraEffectActor : public AActor
 public:
 	AAuraEffectActor();
 
-
 protected:
 	virtual void BeginPlay() override;
 
 	UFUNCTION(BlueprintCallable)
-	void ApplyEffectToTarget(AActor* Target, TSubclassOf<UGameplayEffect> GameplayEffectClass);
+	void ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass);
 
-	UPROPERTY(EditAnywhere, Category = "Effects")
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Effects")
 	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
 };
